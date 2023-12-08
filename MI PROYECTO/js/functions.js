d6b3fd09ca3e7faccd895619e658c37ee2a74fd1
jocaher0503 <jocaher0503@g.educaand.es>
2023-12-08 20:51:30 +0100

    .

---
+++ MI PROYECTO/js/functions.js
@@ -0,0 +1,28 @@
+/********************************************************/
+/*      JS for My PROJECT tribute site                */
+/*      author: Juan Carlos Ocaña Heredia               */
+/*      version. 1.0                                    */
+/*      comment: project for Lenguaje de Marcas - 1ºDAM */
+/********************************************************/
+
+
+/****************** NAV BAR *****************/
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
