93876e5a13cfe8bdff7822b7f41a47c9158b83f5
jocaher0503 <jocaher0503@g.educaand.es>
2023-12-17 01:20:40 +0100

    .

--- MI PROYECTO/js/football.js
+++ MI PROYECTO/js/football.js
@@ -30,21 +30,21 @@ $(document).ready(function(){
 
 /****************** BUTTONS *****************/
 
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
+function myFunction2() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
   }
+}
 
 /****************** NAV BAR *****************/
 
